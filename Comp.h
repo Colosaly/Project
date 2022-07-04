a5605bdcdb789ba63e9a9857d8a37b331bd1bc32
Colosaly <108298018+Colosaly@users.noreply.github.com>
2022-07-04 20:03:46 +0300

    Update Comp.h

--- Comp.h
+++ Comp.h
@@ -11,11 +11,16 @@ class Comp
         Comp();
         Comp(const Comp &obj);
         Comp(int _ip, const std::string& _proc, int _mem);
+        
+        //getters
         int get_ip() const;
         std::string get_proc() const;
         int get_mem() const;
+    
+        //setters
         void set_ip(int new_ip);
         void set_proc(std::string new_proc);
         void set_mem(int new_mem);
+    
         void show();
 };      
