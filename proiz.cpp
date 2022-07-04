6f5ddce2e859779580c00a8ae38a82cab119460f
Диджеешко <108298074+anlemna@users.noreply.github.com>
2022-07-04 20:18:03 +0300

    Add files via upload

---
+++ proiz.cpp
@@ -0,0 +1,21 @@
+﻿#include "Header.h"
+using namespace std;
+
+proiz::proiz() : sernom(0) {}
+proiz::proiz(const proiz& obj) : sernom(obj.sernom), firma(obj.firma), strana(obj.strana) {};
+proiz::proiz(int _sernom, const string& _firma, const string& _strana) : sernom(_sernom), firma(_firma), strana(_strana) {};
+
+int proiz::get_sernom() const { return sernom; }
+string proiz::get_firma() const { return firma; }
+string proiz::get_strana() const { return strana; }
+
+void proiz::set_sernom(int new_sernom) { sernom = new_sernom; }
+void proiz::set_firma(string new_firma) { firma = new_firma; }
+void proiz::set_strana(string new_strana) { strana = new_strana; }
+
+void proiz::show()
+{
+	cout << "Серийный номер: " << sernom << endl;
+	cout << "Фирма: " << firma << endl;
+	cout << "Страна изготовитель: " << strana << endl;
+};
