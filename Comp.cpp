bac8a7cf2ab09e3cfdccc43ef8bbeccdae30805d
Colosaly <108298018+Colosaly@users.noreply.github.com>
2022-07-04 20:04:28 +0300

    Update Comp.cpp

--- Comp.cpp
+++ Comp.cpp
@@ -4,16 +4,20 @@ using namespace std;
 Comp::Comp() : ip(0), mem(0) {}
 Comp::Comp(const Comp& obj) : ip(obj.ip), proc(obj.proc), mem(obj.mem) {};
 Comp::Comp(int _ip, const string& _proc, int _mem) : ip(_ip), proc(_proc), mem(_mem) {};
+
+//getters
 int Comp::get_ip() const { return ip; };
 string Comp::get_proc() const { return proc; };
 int Comp::get_mem() const { return mem; };
+//setters
 void Comp::set_ip(int new_ip) { ip = new_ip; };
 void Comp::set_proc(string new_proc) { proc = new_proc; };
 void Comp::set_mem(int new_mem) { mem = new_mem; };
+
 void Comp::show()
 {
 	cout << "IP: " << ip << endl;
-	cout << "Ïðîöåññîð: " << proc << endl;
-	cout << "Ïàìÿòü: " << mem << endl;
+	cout << "ÃÃ°Ã®Ã¶Ã¥Ã±Ã±Ã®Ã°: " << proc << endl;
+	cout << "ÃÃ Ã¬Ã¿Ã²Ã¼: " << mem << endl;
 };
 
